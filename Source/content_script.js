b5ea5c6e98b44d857e5fbd98bf3387aed309ba70
Matthew Deadman <matthew.deadman@gmail.com>
2016-07-23 14:06:59 -0400

    My name is Francis of the Filth and I am here today to eat aklauhmsdg lago hjha

--- Source/content_script.js
+++ Source/content_script.js
@@ -1,66 +1,75 @@
 walk(document.body);
 
-function walk(node) 
-{
-	// We stole this from Ian fox <3
-	
-	// Ian stole this function from here:
-	// http://is.gd/mwZp7E
-	
-	var child, next;
+function walk(node) {
+    // We stole this from Ian fox <3
 
-	switch ( node.nodeType )  
-	{
-		case 1:  // Element
-		case 9:  // Document
-		case 11: // Document fragment
-			child = node.firstChild;
-			while ( child ) 
-			{
-				next = child.nextSibling;
-				walk(child);
-				child = next;
-			}
-			break;
+    // Ian stole this function from here:
+    // http://is.gd/mwZp7E
 
-		case 3: // Text node
-            if(node.parentElement.tagName.toLowerCase() != "script") {
-                handleText(node);
-            }
-			break;
-	}
+    pikaParty();
+
+    //	var child, next;
+    //
+    //	switch ( node.nodeType )  
+    //	{
+    //		case 1:  // Element
+    //		case 9:  // Document
+    //		case 11: // Document fragment
+    //			child = node.firstChild;
+    //			while ( child ) 
+    //			{
+    //				next = child.nextSibling;
+    //				walk(child);
+    //				child = next;
+    //			}
+    //			break;
+    //
+    //		case 3: // Text node
+    //            if(node.parentElement.tagName.toLowerCase() != "script") {
+    //                handleText(node);
+    //            }
+    //			break;
+    //	}
 }
 
 function handleText(textNode) {
-	var v = textNode.nodeValue;
-	console.log(v);
-	var arr = v.split(" ");
-	var newArr = [];
-	console.log(arr);
-	for (var i = 0; i < arr.length; i++) {
-		var len = arr[i].length;
-		if(!len)
-			break;
-		if(len <= 2)
-			newArr.push("Pi");
-		else if(len == 3)
-			newArr.push("Chu");
-		else if(len <= 5)
-			newArr.push("Pika");
-		else if(len <= 7)
-			newArr.push("Pikachu");
-		else{
-			var longasspikachu = "Pikachuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu";
-			newArr.push(longasspikachu.slice(0, len)); // im just gonna hope thats enough u's
-		}
-	}
-	
-	
-	var retVal = newArr.join(" ");
-	
-	console.log(newArr, "\n", retVal);
+    var v = textNode.nodeValue;
+    console.log(v);
+    var arr = v.split(" ");
+    var newArr = [];
+    console.log(arr);
+    for (var i = 0; i < arr.length; i++) {
+        var len = arr[i].length;
+        if (!len)
+            break;
+        if (len <= 2)
+            newArr.push("Pi");
+        else if (len == 3)
+            newArr.push("Chu");
+        else if (len <= 5)
+            newArr.push("Pika");
+        else if (len <= 7)
+            newArr.push("Pikachu");
+        else {
+            var longasspikachu = "Pikachuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu";
+            newArr.push(longasspikachu.slice(0, len)); // im just gonna hope thats enough u's
+        }
+    }
 
-	textNode.nodeValue = retVal;
+
+    var retVal = newArr.join(" ");
+
+    console.log(newArr, "\n", retVal);
+
+    textNode.nodeValue = retVal;
 }
 
+function pikaParty() {
+    var pikaPop = document.createElement('img');
+    pikaPop.src = chrome.extension.getURL('PikaPop1.png');
+    pikaPop.setAttribute('id', 'pikalatorPop');
 
+    
+    
+    document.body.appendChild(pikaPop);
+}
